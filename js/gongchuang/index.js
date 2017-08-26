7abbe4713977793197c4220019ff884dd1242179
chenlong <chenlongwill@qq.com>
2017-08-26 18:59:09 +0800

    首页完成50%

---
+++ js/gongchuang/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by admin on 2017/8/26.
+ */
+var Index={
+    init:function () {
+        $('#myCarousel').carousel({
+            interval: 2000
+        });
+    },
+
+};
+
+$(document).ready(function () {
+    Index.init();
+});
